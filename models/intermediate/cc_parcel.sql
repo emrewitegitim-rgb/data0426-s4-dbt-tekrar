fb9fe142afe0e48fa493af59a680d99d72b5e78f
emre.wit.egitim@gmail.com <emre.wit.egitim@gmail.com>
2026-04-30 10:01:55 +0000

    feat: add status to cc_parcel

--- models/intermediate/cc_parcel.sql
+++ models/intermediate/cc_parcel.sql
@@ -1,4 +1,10 @@
 SELECT
     p.*
     , EXTRACT(MONTH FROM date_purchase) as month_purchase
+    , CASE
+        WHEN date_cancelled is not null THEN 'Cancelled'
+        WHEN date_delivery is not null THEN 'Delivered'
+        WHEN date_shipping is not null THEN 'Shipped'
+        ELSE 'In porgress'
+    END as status
 FROM {{ ref("stg_raw__parcel")}} as p
